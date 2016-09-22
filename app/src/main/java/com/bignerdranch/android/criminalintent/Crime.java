3fdc36a63bd91ea3620a268096548c8f7f583d76
miked <whatthafunk3@aol.com>
2016-09-22 02:51:12 -0500

    Initial commit, all of chapter 7 included

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,27 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by miked on 9/22/2016.
+ */
+
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime (){
+        //Generate unique identifier
+        mId = UUID.randomUUID();
+    }
+    public UUID getId(){
+        return mId;
+    }
+    public String getTitle(){
+        return mTitle;
+    }
+    public void setTitle(String title){
+        mTitle = title;
+    }
+}
